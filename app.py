a08d7c392a283cb20ec9643533062216548834eb
Monisha <pmonisha5328@gmail.com>
2026-08-03 16:38:22 +0530

    Added resume PDF parsing

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
 import streamlit as st
+from src.parser import extract_text
 
-st.set_page_config(page_title="AI Referral Finder")
+st.set_page_config(
+    page_title="AI Referral Finder",
+    layout="wide"
+)
 
 st.title("🤖 AI Referral Finder & Outreach Assistant")
-st.write("Welcome to my AI project!")
+
+resume = st.file_uploader(
+    "Upload Resume",
+    type=["pdf"]
+)
+
+job_description = st.text_area(
+    "Paste Job Description",
+    height=250
+)
+
+if st.button("Analyze"):
+
+    if resume:
+
+        resume_text = extract_text(resume)
+
+        st.subheader("Extracted Resume Text")
+
+        st.text_area(
+            "Resume Content",
+            resume_text,
+            height=300
+        )
+
+    else:
+        st.warning("Please upload a resume.")
